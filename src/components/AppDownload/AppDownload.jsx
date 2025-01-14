0b25e39bdb2c9be89a5f819e9082a476ab47f720
Fathima Shafrin pt <105667969+shafrinpt@users.noreply.github.com>
2025-01-14 20:59:16 +0530

    change foe to for

--- src/components/AppDownload/AppDownload.jsx
+++ src/components/AppDownload/AppDownload.jsx
@@ -5,7 +5,7 @@ import { assets } from '../../assets/assets'
 const AppDownload = () => {
   return (
     <div className='app-download' id='app-download'>
-    <p>Foe Better Experience Download <br/> Dine-dash App </p>
+    <p>For Better Experience Download <br/> Dine-dash App </p>
     <div className="app-download-platforms">
         <img src={assets.play_store} alt="" />
         <img src={assets.app_store} alt="" />
